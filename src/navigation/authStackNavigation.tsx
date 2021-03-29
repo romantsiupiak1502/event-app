510cacffaad35a6a5fbceaccd423a12b375a375d
romantsiupiak1502 <roman.ts@nerdysoft.com>
2021-03-29 16:10:02 +0300

    Fix navigation

---
+++ src/navigation/authStackNavigation.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Greeting, Login, Registration } from "../containers";
+
+const Stack = createStackNavigator();
+
+export const AuthStackNavigation = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={"Greeting"}
+          options={{headerShown: false}}
+          component={Greeting}
+        />
+        <Stack.Screen
+          name={"Login"}
+          options={{headerShown: false}}
+          component={Login}
+        />
+        <Stack.Screen
+          name={"Registration"}
+          options={{headerShown: false}}
+          component={Registration}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
